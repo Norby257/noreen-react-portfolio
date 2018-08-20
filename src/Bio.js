753acea94160e48cbeaad0def961dc9c45afb46d
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-08-19 21:18:43 -0500

    issue 2: Bio component

---
+++ src/Bio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Bio(props) {
+   return (
+       <div>
+           <img />
+           <p> Hi, I'm Noreen. I'm a full stack developer and I specialize in React. </p>
+           <p> I've also worked in analytics, and I am passionate about data-driven fraud prevention.</p>
+           <p> When I take a break from coding, I can be caught playing guitar, enjoying combat sports or consuming matcha </p>
+        </div>
+   )
+}
+
+
+export default Bio;
