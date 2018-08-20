1f96bb7cad2dd6e239cddb525d802f5e7fca833f
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-08-19 21:30:07 -0500

    issue 2: Contact component

---
+++ src/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Contact(props) {
+    return (
+        <div>
+            <p> React out to me about software. I enjoy learning about new libraries and technologies. </p>
+            <div>
+            <a href="https://github.com/Norby257" rel="noopener noreferrer" target="_blank"> <i className="fab fa-github"></i></a>
+    
+   <a href="https://linkedin.com/in/nmaddenling/" rel="noopener noreferrer" target="_blank"> <i className="fab fa-linkedin"></i></a>
+            </div>
+        </div>
+    )
+
+}
+
+
+export default Contact; 
