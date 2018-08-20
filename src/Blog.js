7ec8f5b9233669a2a93668663241a6eb25839cc0
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-08-19 21:34:58 -0500

    issue 2: Blog Component

---
+++ src/Blog.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Blog(props) {
+    return (
+        <div>
+            <p>
+                Here's some blogs I've written: 
+            </p>
+            <p>
+                Bonus: Here's a techical paper that I contributed to:
+            </p>
+        </div>
+    )
+}
+
+
+export default Blog;
