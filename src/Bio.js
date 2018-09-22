4072ec891059bfde7b58998cc71f2c3653358431
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-09-22 09:35:28 -0500

    project list complete

--- src/Bio.js
+++ src/Bio.js
@@ -4,7 +4,7 @@ function Bio(props) {
    return (
        <div>
            <img  alt="profile pic"/>
-           <p> Hi, I'm Noreen. I'm a full stack developer and I specialize in React. </p>
+           <p> Hi, I'm Noreen. I'm a full stack web developer and I specialize in React. </p>
            <p> I've also worked in analytics, and I am passionate about data-driven fraud prevention.</p>
            <p> When I take a break from coding, I can be caught playing guitar, enjoying combat sports or consuming matcha. </p>
         </div>
